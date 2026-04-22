8efbfe27c56df477df19c394dfe3607d0501439b
feeblescholar1 <btndk6@gmail.com>
2026-04-22 19:51:19 +0200

    Matrix_Error kiszervezése külön fájlokba.

--- genMatrix.hpp
+++ genMatrix.hpp
@@ -476,39 +476,6 @@ namespace genMatrix {
         bool operator<(const Matrix_Iterator& rhs_iter) const;
 
     };
-
-    /**
-     * @class Matrix_Error
-     * @brief A Matrix osztály hibaosztálya.
-     * @note Mivel a Matrix osztály egy template, a fordítási idő csökkentése érdekében nem az "eredeti" osztály része.
-     */
-    class Matrix_Error : public std::exception {
-        bool isFatal;   /** Kritikus hiba-e? */
-        char *src_func; /** Függvény neve, ahol a hiba történt */
-        char *msg;      /** Hibaüzenet */
-
-    public:
-        /**
-         * @param _src A függvény neve, ahol a hiba keletkezett.
-         * @param _msg A hibaüzenet
-         * @param fatal Kritikus-e a hiba? (ki kell-e lépni), alapértelmezetten hamis
-         */
-        Matrix_Error(const char *_src, const char *_msg, bool fatal = false);
-
-        Matrix_Error(const Matrix_Error& other);
-
-        /**
-         * @return A függvény neve, ahol a hiba történt (0-val lezárt C string).
-         */
-        const char *src() const noexcept;
-
-        /**
-         * @return A hibaüzenet (0-val lezárt C string).
-         */
-        const char* what() const noexcept override;
-
-        ~Matrix_Error();
-    };
 }
 
 #endif
---
+++ matrix_error.cpp
@@ -0,0 +1,36 @@
+#include "matrix_error.hpp"
+
+using namespace genMatrix;
+
+Matrix_Error::Matrix_Error(const char *_src, const char *_msg, bool fatal = false) : isFatal(fatal), src_func(nullptr), msg(nullptr) {
+    src_func = new char[std::strlen(_src) + 1];
+    msg = new char[std::strlen(_msg) + 1];
+
+    std::strcpy(msg, _msg);
+    std::strcpy(src_func, _src);
+}
+
+Matrix_Error::Matrix_Error(const Matrix_Error& other) : isFatal(false), src_func(nullptr), msg(nullptr) {
+    src_func = new char[std::strlen(other.src_func) + 1];
+    msg = new char[std::strlen(other.msg) + 1];
+    isFatal = other.isFatal;
+
+    std::strcpy(msg, other.msg);
+    std::strcpy(src_func, other.src_func);
+}
+
+
+const char* Matrix_Error::src() const {
+    return src_func;
+}
+
+const char* Matrix_Error::what() const {
+    return msg;
+}
+
+Matrix_Error::~Matrix_Error() {
+    delete[] src_func;
+    delete[] msg;
+
+    msg = src_func = nullptr;
+}
