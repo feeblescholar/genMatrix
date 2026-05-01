078232867731d242625629240d51f4a8160ae32f
feeblescholar1 <btndk6@gmail.com>
2026-05-01 07:41:41 +0200

    C++17 standard check.

--- src/include/genMatrix.hpp
+++ src/include/genMatrix.hpp
@@ -4,18 +4,21 @@
  * @brief Main header file a genMatrixhoz.
  */
 #ifndef GENMATRIX
-#define GENMATRIX
+#if __cplusplus >= 201703L
+    #define GENMATRIX
 
-#include "type_func.hpp"
+    #include "type_func.hpp"
 
-#include "matrix.hpp"
-#include "matrix_commaInit.hpp"
-#include "matrix_error.hpp"
+    #include "matrix.hpp"
+    #include "matrix_commaInit.hpp"
+    #include "matrix_error.hpp"
 
-#include "determinant.hpp"
-#include "norms.hpp"
-#include "fhandler.hpp"
+    #include "determinant.hpp"
+    #include "norms.hpp"
+    #include "fhandler.hpp"
 
-#include "vector.hpp"
-
+    #include "vector.hpp"
+#else
+    #error "This library requires C++17 or never";
+#endif
 #endif
